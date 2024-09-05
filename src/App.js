7ee133a5902889e17f48054a02f3dc22365ccdf0
iampravinsubramaniyam <pravinsubramaniyam@gmail.com>
2024-09-05 17:48:19 +0530

    loading page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './Home.js';
+import Loading from './components/Loading.js';
 
 function App() {
+
+  const [loading,setLoading] = useState(true);
+
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },2000)
+  },[])
+
   return (
+    loading ? (<Loading/>):(
     <div className="App">
         <Home/>
-    </div>
+    </div>)
   );
 }
 
---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/loading.css"
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+        <h1 style={{fontWeight: "500", color:"rgb(14, 140, 157)"}}>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
