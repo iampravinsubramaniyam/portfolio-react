94d89cb64d3553e7d7684cb73846f6bf8b8e8cf8
iampravinsubramaniyam <pravinsubramaniyam@gmail.com>
2024-09-07 11:34:46 +0530

    resume indicator added

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -9,6 +9,8 @@ import cloud from '../images/main/cloud.png';
 import boat from '../images/main/boat.png';
 import WheelContainer from '../components/WheelContainer';
 import LastUpdated from '../components/LastUpdated';
+import { FaCaretRight } from "react-icons/fa";
+import resume from "../resume/PRAVEEN S (BTECH - IT)- RESUME.pdf"
 
 const Main = ({ setMainInView}) => {
   const main = useRef(null);
@@ -31,9 +33,12 @@ const Main = ({ setMainInView}) => {
 
       <div className="cloud-container">
         <div className="resume-container">
-          <a className='resume-cloud' href={cloud} download>
+          <a className='resume-cloud' href={resume} download>
             <img src={cloud} alt="Download CV" />
           </a>
+          <div className="resume-indication">
+            Resume <FaCaretRight className='resume-indicator'/>
+          </div>
         </div>
         <img src={clouds} alt="clouds" />
       </div>
