f5dba08657d261e1aff603f8cb75081cf39c6b92
iampravinsubramaniyam <pravinsubramaniyam@gmail.com>
2024-09-12 10:39:38 +0530

    styles changed

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { useInView } from 'framer-motion';
 import "../styles/about.css";
 import { skills } from '../data/data.js';
@@ -7,9 +7,11 @@ import BubblesAnimation from '../components/BubblesAnimation.js';
 
 const About = ({ setAboutInView }) => {
   const tech = useRef(null);
-  const about = useRef(null)
+  const about = useRef(null);
+  const [isStoneLoaded, setStoneLoaded] = useState(false);
+
   const aboutStatus = useInView(about);
-  const techStatus = useInView(tech)
+  const techStatus = useInView(tech);
 
   useEffect(() => {
     setAboutInView(aboutStatus);
@@ -19,7 +21,9 @@ const About = ({ setAboutInView }) => {
     <div className='about' id='about' ref={about}>
       <div className="about-wraper">
         <div className="content">
-          <div className= {`about-heading ${aboutStatus ? "show-about-heading":"hide-about-heading"}`}>About Me</div>
+          <div className={`about-heading ${aboutStatus ? "show-about-heading" : "hide-about-heading"}`}>
+            About Me
+          </div>
           <p className="para">
             Hey!<br /><br />
             I am a recent graduate with a strong foundation in computer science and engineering. My expertise lies in programming languages such as C and C++, where I have honed my skills in algorithm design and data structures. Additionally, I am proficient in web development technologies, including HTML, CSS, and JavaScript, allowing me to create dynamic and responsive web applications. I also have experience with React for building user interfaces and Java with Spring Boot, Hibernate, and JPA for backend development. My knowledge extends to database management with MySQL, enabling me to design and manage robust data systems.
@@ -33,18 +37,25 @@ const About = ({ setAboutInView }) => {
             <h3 className="skillSetHeading">Tech Stack</h3>
             <ul className='tech-skills-wraper' ref={tech}>
               {skills.map((skill, index) => (
-                <li key={index}  className = {`techskills ${techStatus?"show-tech-status" : "hide-tech-status"}`}>
-                  {/* <img src={skill.path} alt={skill.name} /> */}
+                <li
+                  key={index}
+                  className={`techskills ${techStatus ? "show-tech-status" : "hide-tech-status"}`}
+                >
+                  {/* Render skill icon */}
                   {skill.icon}
                 </li>
               ))}
             </ul>
           </div>
         </div>
-
       </div>
 
-      <img className='stone-wall' src={stone} alt="stone" />
+      <img
+        className={`stone-wall ${!isStoneLoaded ? 'blur' : 'loaded'}`}
+        src={stone}
+        alt="stone wall"
+        onLoad={() => setStoneLoaded(true)}
+      />
       <BubblesAnimation />
     </div>
   );
--- src/sections/Main.js
+++ src/sections/Main.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "../styles/main.css";
 import AutoType from '../components/AutoType';
 import { useInView } from 'framer-motion';
 
-import sea from "../images/main/sea.png"
+import sea from "../images/main/sea.png";
 import clouds from '../images/main/clouds.png';
 import cloud from '../images/main/cloud.png';
 import boat from '../images/main/boat.png';
 import WheelContainer from '../components/WheelContainer';
 import LastUpdated from '../components/LastUpdated';
 import { FaCaretRight } from "react-icons/fa";
-import resume from "../resume/PRAVEEN S (BTECH - IT)- RESUME.pdf"
+import resume from "../resume/PRAVEEN S (BTECH - IT)- RESUME.pdf";
 
-const Main = ({ setMainInView}) => {
+const Main = ({ setMainInView }) => {
+  const [isSeaLoaded, setSeaLoaded] = useState(false);
+  const [isCloudsLoaded, setCloudsLoaded] = useState(false);
+  const [isCloudLoaded, setCloudLoaded] = useState(false);
+  const [isBoatLoaded, setBoatLoaded] = useState(false);
+
   const main = useRef(null);
   const mainStatus = useInView(main);
 
   useEffect(() => {
     setMainInView(mainStatus);
   }, [mainStatus, setMainInView]);
-
-
 
   return (
     <div className='main' id="home">
@@ -29,22 +32,42 @@ const Main = ({ setMainInView}) => {
         <div className="autospell">I'm <AutoType /></div>
       </div>
 
-      <WheelContainer main = {main} content={<LastUpdated />}/>
+      <WheelContainer main={main} content={<LastUpdated />} />
 
       <div className="cloud-container">
         <div className="resume-container">
           <a className='resume-cloud' href={resume} download>
-            <img src={cloud} alt="Download CV" />
+            <img
+              src={cloud}
+              alt="Download CV"
+              className={!isCloudLoaded ? 'blur' : ''}
+              onLoad={() => setCloudLoaded(true)}
+            />
           </a>
           <div className="resume-indication">
-            Resume <FaCaretRight className='resume-indicator'/>
+            Resume <FaCaretRight className='resume-indicator' />
           </div>
         </div>
-        <img src={clouds} alt="clouds" />
+        <img
+          src={clouds}
+          alt="clouds"
+          className={!isCloudsLoaded ? 'blur' : ''}
+          onLoad={() => setCloudsLoaded(true)}
+        />
       </div>
 
-      <img className='boat' src={boat} alt="boat" />
-      <img className="sea" src={sea} alt="sea" />
+      <img
+        className={`boat ${!isBoatLoaded ? 'blur' : ''}`}
+        src={boat}
+        alt="boat"
+        onLoad={() => setBoatLoaded(true)}
+      />
+      <img
+        className={`sea ${!isSeaLoaded ? 'blur' : ''}`}
+        src={sea}
+        alt="sea"
+        onLoad={() => setSeaLoaded(true)}
+      />
     </div>
   );
 };
